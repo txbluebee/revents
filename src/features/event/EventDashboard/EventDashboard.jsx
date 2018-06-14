a6cf2d2d201aa3263f860865c82d734c9483709e
Brian Fan <kuomu.fan@yahoo.com>
2018-06-14 10:21:03 -0700

    add function to handle newEvent submission

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Button } from "semantic-ui-react";
+import cuid from 'cuid';
 import EventList from "../EventList/EventList";
 import EventForm from "./../EventForm/EventForm";
 
@@ -73,6 +74,16 @@ class EventDashboard extends Component {
     })
   }
 
+  handleCreateEvent = (newEvent) =>{
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = './assets/user.png';
+    const updateEvents = [...this.state.events, newEvent];
+    this.setState({
+      events: updateEvents,
+      isOpen: false
+    })
+  }
+
   render() {
     return (
       <Grid>
@@ -82,7 +93,7 @@ class EventDashboard extends Component {
         <Grid.Column width={6}>
           <Button onClick={this.handleFormOpen} positive content="Create Event" />
           {this.state.isOpen && 
-          <EventForm handleCancel={this.handleCancel}/>}
+          <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel}/>}
         </Grid.Column>
       </Grid>
     );
