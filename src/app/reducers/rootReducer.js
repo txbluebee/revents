a538afa34d9f0c0331661fd49fadbac78de6af3b
Brian Fan <kuomu.fan@yahoo.com>
2018-06-19 14:01:28 -0700

    integrated redux form

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
+import { reducer as FormReducdr } from 'redux-form';
 import testReducer from '../../features/textarea/testReducer';
 import eventReducer from '../../features/event/eventReducer'
 
 const rootReducer = combineReducers({
+  form: FormReducdr,
   test: testReducer,
   events: eventReducer
 })
